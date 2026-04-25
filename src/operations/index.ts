378ac2f63df6231b923f3e85446c9e1e3a96bdee
William Khoo <william_khoo@outlook.com>
2026-04-26 00:29:51 +0800

    feat(adr-019-c): add buildHopCallback factory

--- src/operations/index.ts
+++ src/operations/index.ts
@@ -1,4 +1,6 @@
 export { callOp } from "./call";
+export { buildHopCallback, _buildHopCallbackDeps } from "./build-hop-callback";
+export type { BuildHopCallbackContext } from "./build-hop-callback";
 export { classifyRouteOp } from "./classify-route";
 export type { ClassifyRouteInput, ClassifyRouteOutput } from "./classify-route";
 export type {
