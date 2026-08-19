a8c53cac80268661802dda732b3f3141639bf4bf
Test <william_khoo@outlook.com>
2026-08-19 12:03:09 +0800

    feat(finish): deliver escalations through the forge natively

--- src/finish/index.ts
+++ src/finish/index.ts
@@ -58,6 +58,8 @@ export { _finishPrDeps, loadFinishPrContext } from "./pr";
 export type { FinishPrContext, FinishPrStory, LoadPrContextArgs } from "./pr";
 export { buildFinishBody, buildFinishTitle } from "./pr";
 export { openDraftFinishPr, openOrPromotePr, parseView, updatePrBody } from "./pr";
+export { buildEscalationComment, postEscalation } from "./escalate";
+export type { EscalationOutcome } from "./escalate";
 export {
   gateCommitRoute,
   MAX_FIX_ATTEMPTS,
